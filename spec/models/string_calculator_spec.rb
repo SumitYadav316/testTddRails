77f99768807087f85bcc01d0e0d1ffd4c902548f
sumit yadav <sumityadavna@gmail.com>
2024-06-29 14:42:54 +0530

    returns the number for a single number

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe StringCalculator do
       expect(calculator.add("")).to eq(0)
     end
 
+    it 'returns the number for a single number' do
+      expect(calculator.add("1")).to eq(1)
+    end
+
     it 'throws an exception for negative numbers' do
       expect { calculator.add("1,-2,3,-4") }.to raise_error(RuntimeError, "negative numbers not allowed: -2,-4")
     end
