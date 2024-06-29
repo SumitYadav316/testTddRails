4ba180541d560a899eef4d44088303f26ade9286
sumit yadav <sumityadavna@gmail.com>
2024-06-29 14:45:39 +0530

    supports different delimiters

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -21,6 +21,10 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
 
+    it 'supports different delimiters' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
+
     it 'throws an exception for negative numbers' do
       expect { calculator.add("1,-2,3,-4") }.to raise_error(RuntimeError, "negative numbers not allowed: -2,-4")
     end
